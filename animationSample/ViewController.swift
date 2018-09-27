5bfaced93973683bcaeaf65ed6475431915fdd2f
JW_Macbook <xodhks_0113@naver.com>
2018-09-27 23:02:16 +0900

    Frame Animation View Commit

--- animationSample/ViewController.swift
+++ animationSample/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frameImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setFrameImageView()
     }
-
-
+    
+    
+    /// Frame Animation
+    func setFrameImageView() {
+        /// ImageArray init
+        let frameImageArray = NSMutableArray()
+        for i in 1..<21 {
+            frameImageArray.add(UIImage.init(contentsOfFile: Bundle.main.path(forResource: "icon\(i)@2x", ofType: "png")! ) as Any)
+        }
+        
+        frameImageView.animationImages = frameImageArray as? [UIImage]
+        frameImageView.animationDuration = 1.25
+        frameImageView.animationRepeatCount = 0 // 0 means infinite (default is 0)
+        frameImageView.startAnimating()
+    }
 }
 
